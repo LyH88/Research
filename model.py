5e0a66de70cdafdaefd5aa34fb12de5c9bc4f601
LyH88 <42681057+LyH88@users.noreply.github.com>
2019-06-26 13:41:33 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -84,36 +84,47 @@ class Model(nn.Module):
 class DeformModel2(nn.Module):
     # nclasses=363 - ALL
     # step5
-    def __init__(self, mesh_folder, feat=16, nclasses=2):
+    def __init__(self, mesh_folder, feat=16, nclasses=1):
         super().__init__()
-        mf = os.path.join(mesh_folder, "icosphere_6.pkl")
-        self.in_conv = MeshConv(1, feat, mesh_file=mf, stride=1)
+        mf = os.path.join(mesh_folder, "icosphere_5.pkl")
+        self.in_conv = MeshConv(2, feat, mesh_file=mf, stride=2)
         self.in_bn = nn.BatchNorm1d(feat)
         self.relu = nn.ReLU(inplace=True)
         self.in_block = nn.Sequential(self.in_conv, self.in_bn, self.relu)
-        self.block1 = ResBlock(in_chan=feat, neck_chan=feat, out_chan=4 * feat, level=4, coarsen=True,
-                               mesh_folder=mesh_folder)
-        self.block2 = ResBlock(in_chan=4 * feat, neck_chan=4 * feat, out_chan=16 * feat, level=3, coarsen=True,
-                               mesh_folder=mesh_folder)
-        #self.block3 = ResBlock(in_chan=16 * feat, neck_chan=16 * feat, out_chan=64 * feat, level=2, coarsen=True, mesh_folder=mesh_folder)
-        #self.avg = nn.AvgPool1d(kernel_size=self.block3.nv_prev)  # output shape batch x channels x 1
-        #self.out_layer = nn.Linear(64 * feat, nclasses)
 
+        self.block1 = ResBlock(in_chan=feat, neck_chan=feat, out_chan=4 * feat, level=3, coarsen=True,
+                               mesh_folder=mesh_folder)
+        self.block2 = ResBlock(in_chan=4 * feat, neck_chan=4 * feat, out_chan=16 * feat, level=2, coarsen=True,
+                               mesh_folder=mesh_folder)
         self.avg = nn.AvgPool1d(kernel_size=self.block2.nv_prev)  # output shape batch x channels x 1
-        self.out_layer = nn.Linear(16 * feat, nclasses) # replaced: nclasses
+        self.out_layer = nn.Linear(16 * feat, nclasses)  # replaced: nclasses
+
+
+        # self.block1 = ResBlock(in_chan=feat, neck_chan=feat, out_chan=4 * feat, level=4, coarsen=True,
+        #                        mesh_folder=mesh_folder)
+        # self.block2 = ResBlock(in_chan=4 * feat, neck_chan=4 * feat, out_chan=16 * feat, level=3, coarsen=True,
+        #                        mesh_folder=mesh_folder)
+        # self.block3 = ResBlock(in_chan=16 * feat, neck_chan=16 * feat, out_chan=64 * feat, level=2, coarsen=True,
+        #                        mesh_folder=mesh_folder)
+        # self.avg = nn.AvgPool1d(kernel_size=self.block3.nv_prev)  # output shape batch x channels x 1
+        # self.out_layer = nn.Linear(64 * feat, nclasses)
+
+
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.in_block(x)
         x = self.block1(x)
         x = self.block2(x)
-        #x = self.block3(x)
-        #x = torch.squeeze(self.avg(x), dim=-1)
+
+        # x = self.block3(x)
+        # x = torch.squeeze(self.avg(x), dim=-1)
 
         x = torch.squeeze(self.avg(x))
-        x = F.dropout(x, training=self.training)
+
+        #x = F.dropout(x, training=self.training)
         x = self.out_layer(x)
         x = self.sigmoid(x)
 
-        #return F.log_softmax(x, dim=1)
+        #return F.softmax(x, dim=1)
         return x
