f4c6811b64bd18196711f49363588f049d7e2aba
LyH88 <42681057+LyH88@users.noreply.github.com>
2019-06-26 13:41:08 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ from torch.utils.data import Dataset
 import pickle, gzip
 import os
 import pandas as pd
+from os.path import join
+from joblib import Parallel, delayed
+import multiprocessing
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -84,6 +87,7 @@ class MNIST_S2_Loader(Dataset):
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
 
+
 class Test_Loader(Dataset):
     """Data loader for testing."""
 
@@ -97,43 +101,43 @@ class Test_Loader(Dataset):
         lev_sphere = [12, 42, 162, 642, 2562, 10242, 40962, 163842]
         self.level = lev_sphere[6]
         if partition == "train":
-            label, matrix = self.readin(data_dir, 'CTtrain.txt', '/home/haol/Python Script/CTtraintable.csv')
-            #label2, matrix2 = self.readin(data_dir, 'Htrain.txt', '/home/haol/Python Script/Htraintable.csv')
-            #label3, matrix3 = self.readin(data_dir, 'SDtrain.txt', '/home/haol/Python Script/SDtraintable.csv')
-            #label4, matrix4 = self.readin(data_dir, 'LGItrain.txt', '/home/haol/Python Script/LGItraintable.csv')
-            #label5, matrix5 = self.readin(data_dir, 'Strain.txt', '/home/haol/Python Script/Straintable.csv')
-            #label6, matrix6 = self.readin(data_dir, 'SCItrain.txt', '/home/haol/Python Script/SCItraintable.csv')
-            # x = np.concatenate((matrix, matrix6),1)
-            # self.y = label
-            # self.x = x
+            label, matrix = self.readin(data_dir, 'CTtrain.txt', '/home/haol/Python Script/CV6/CTtraintable.csv')
+            #label2, matrix2 = self.readin(data_dir, 'Htrain.txt', '/home/haol/Python Script/CV2/Htraintable.csv')
+            #label3, matrix3 = self.readin(data_dir, 'SDtrain.txt', '/home/haol/Python Script/CV2/SDtraintable.csv')
+            #label4, matrix4 = self.readin(data_dir, 'LGItrain.txt', '/home/haol/Python Script/CV1/LGItraintable.csv')
+            #label5, matrix5 = self.readin(data_dir, 'Strain.txt', '/home/haol/Python Script/CV2/Straintable.csv')
+            #label6, matrix6 = self.readin(data_dir, 'SCItrain.txt', '/home/haol/Python Script/CV3/SCItraintable.csv')
+            #x = np.concatenate((matrix, matrix6),1)
+            #self.y = label
+            #self.x = x
 
             self.x = matrix
             self.y = label
 
         elif partition == "test":
-            label, matrix = self.readin(data_dir, 'CTtest.txt','/home/haol/Python Script/CTtesttable.csv')
-            #label2, matrix2 = self.readin(data_dir, 'Htest.txt', '/home/haol/Python Script/Htesttable.csv')
-            #label3, matrix3 = self.readin(data_dir, 'SDtest.txt', '/home/haol/Python Script/SDtesttable.csv')
-            #label4, matrix4 = self.readin(data_dir, 'LGItest.txt', '/home/haol/Python Script/LGItesttable.csv')
-            #label5, matrix5 = self.readin(data_dir, 'Stest.txt', '/home/haol/Python Script/Stesttable.csv')
-            # label6, matrix6 = self.readin(data_dir, 'SCItest.txt', '/home/haol/Python Script/SCItesttable.csv')
-            # x = np.concatenate((matrix, matrix6),1)
-            # self.y = label
-            # self.x = x
+            label, matrix = self.readin(data_dir, 'CTtest.txt','/home/haol/Python Script/CV6/CTtesttable.csv')
+            #label2, matrix2 = self.readin(data_dir, 'Htest.txt', '/home/haol/Python Script/CV2/Htesttable.csv')
+            #label3, matrix3 = self.readin(data_dir, 'SDtest.txt', '/home/haol/Python Script/CV2/SDtesttable.csv')
+            #label4, matrix4 = self.readin(data_dir, 'LGItest.txt', '/home/haol/Python Script/CV1/LGItesttable.csv')
+            #label5, matrix5 = self.readin(data_dir, 'Stest.txt', '/home/haol/Python Script/CV2/Stesttable.csv')
+            #label6, matrix6 = self.readin(data_dir, 'SCItest.txt', '/home/haol/Python Script/CV3/SCItesttable.csv')
+            #x = np.concatenate((matrix, matrix6),1)
+            #self.y = label
+            #self.x = x
 
             self.x = matrix
             self.y = label
 
         else:
-            label, matrix = self.readin(data_dir, 'CTvalidation.txt', '/home/haol/Python Script/CTvalidationtable.csv')
-            # label2, matrix2 = self.readin(data_dir, 'Hvalidation.txt', '/home/haol/Python Script/Hvalidationtable.csv')
-            # label3, matrix3 = self.readin(data_dir, 'SDvalidation.txt', '/home/haol/Python Script/SDvalidationtable.csv')
-            # label4, matrix4 = self.readin(data_dir, 'LGIvalidation.txt', '/home/haol/Python Script/LGIvalidationtable.csv')
-            # label5, matrix5 = self.readin(data_dir, 'Svalidation.txt', '/home/haol/Python Script/Svalidationtable.csv')
-            # label6, matrix6 = self.readin(data_dir, 'SCIvalidation.txt', '/home/haol/Python Script/SCIvalidationtable.csv')
-            # x = np.concatenate((matrix, matrix6), 1)
-            # self.y = label
-            # self.x = x
+            label, matrix = self.readin(data_dir, 'CTvalidation.txt', '/home/haol/Python Script/CV6/CTvalidationtable.csv')
+            # label2, matrix2 = self.readin(data_dir, 'Hvalidation.txt', '/home/haol/Python Script/CV2/Hvalidationtable.csv')
+            # label3, matrix3 = self.readin(data_dir, 'SDvalidation.txt', '/home/haol/Python Script/CV2/SDvalidationtable.csv')
+            #label4, matrix4 = self.readin(data_dir, 'LGIvalidation.txt', '/home/haol/Python Script/CV1/LGIvalidationtable.csv')
+            # label5, matrix5 = self.readin(data_dir, 'Svalidation.txt', '/home/haol/Python Script/CV2/Svalidationtable.csv')
+            #label6, matrix6 = self.readin(data_dir, 'SCIvalidation.txt', '/home/haol/Python Script/CV3/SCIvalidationtable.csv')
+            #x = np.concatenate((matrix, matrix6), 1)
+            #self.y = label
+            #self.x = x
 
             self.x = matrix
             self.y = label
@@ -145,7 +149,6 @@ class Test_Loader(Dataset):
         print(self.x.dtype)
         print(self.y.dtype)
 
-
     def __len__(self):
         return len(self.y)
 
@@ -154,13 +157,12 @@ class Test_Loader(Dataset):
 
     def readin(self, data_dir, spec_dir, csv_path):
         label = pd.read_csv(csv_path)['dx_group'].values.astype(np.int64) #[0:20]
-        #label = np.expand_dims(label, 1)
         full_dir = data_dir + spec_dir
         with open(full_dir) as f:
             dir = f.readlines()
         d = [w.replace('\n', '') for w in dir]
         H_ = np.array([])
-        for i in range(len(d)): #20
+        for i in range(len(d)): #20 len(d)
             if i % 5 == 0:
                 print(i)
             H_ = np.concatenate([H_, np.loadtxt(fname=d[i])])
@@ -168,3 +170,140 @@ class Test_Loader(Dataset):
         H_ = H_[:, :self.level].astype(np.float32)
         H_ = np.expand_dims(H_, 1)
         return label, H_
+
+
+class Better_Loader(Dataset):
+    """Data loader for dataset."""
+
+    def __init__(self, data_dir, cv, td, asd, partition="train"):  # data_dir = "/fs4/masi/haol/data/"
+        """
+        Args:
+            data_dir: path to the folder containing all datafiles
+            partition: train or test
+        """
+        assert (partition in ["train", "test", "validation"])
+        lev_sphere = [12, 42, 162, 642, 2562, 10242, 40962, 163842]
+        self.level = lev_sphere[5]
+
+        # table = pd.read_csv('/home/haol/Python Script/updated_table.csv')
+        # td = table[table['dx_group'] == 0]
+        # asd = table[table['dx_group'] == 1]
+        # td = td.sample(frac=1).reset_index(drop=True)
+        # asd = asd.sample(frac=1).reset_index(drop=True)
+        train, test, valid = self.k_folds(cv, td, asd)
+
+        if partition == "train":
+            trainfile = train['Session'].values.astype(str)
+            label = train['dx_group'].values.astype(np.int64)
+            matrix = self.readin(data_dir, '.lh.CT.txt', trainfile)
+            #matrix2 = self.readin(data_dir, '.rh.CT.txt', trainfile)
+            self.x = matrix
+            self.y = label
+
+            # x = np.concatenate((matrix, matrix2),1)
+            # self.y = label
+            # self.x = x
+        elif partition == "test":
+            testfile = test['Session'].values.astype(str)
+            label = test['dx_group'].values.astype(np.int64)
+            matrix = self.readin(data_dir, '.lh.CT.txt', testfile)
+            #matrix2 = self.readin(data_dir, '.rh.CT.txt', testfile)
+            self.x = matrix
+            self.y = label
+
+            # x = np.concatenate((matrix, matrix2), 1)
+            # self.y = label
+            # self.x = x
+        else:
+            validfile = valid['Session'].values.astype(str)
+            label = valid['dx_group'].values.astype(np.int64)
+            matrix = self.readin(data_dir, '.lh.CT.txt', validfile)
+            #matrix2 = self.readin(data_dir, '.rh.CT.txt', validfile)
+            self.x = matrix
+            self.y = label
+
+            # x = np.concatenate((matrix, matrix2), 1)
+            # self.y = label
+            # self.x = x
+
+        print(np.shape(self.x))
+        print(np.shape(self.y))
+
+    def readin(self, data_dir, channel_name, session):  # channel_name: '.CT.txt', session: trainfile
+        H = np.array([])
+        for i, name in enumerate(session):
+            if i % 5 == 0:
+                print(i)
+            f = join(data_dir, name) + channel_name
+            H = np.concatenate([H, np.loadtxt(fname=f)])
+        H = np.reshape(H, (-1, 163842))
+        H = H[:, :self.level].astype(np.float32)
+
+        H = np.expand_dims(H, 1)    # should be expanded if multi-channel
+        return H
+
+    # def readin(self, data_dir, channel_name, session):  # channel_name: '.CT.txt', session: trainfile
+    #     # for i, name in enumerate(session):
+    #     #     H = self.process(i, name)
+    #     H = [None] * len(session)
+    #     #num_cores = multiprocessing.cpu_count()
+    #     H = Parallel(n_jobs=18, require='sharedmem')(delayed(self.process)(i, name, data_dir, channel_name, H) for i, name in enumerate(session))
+    #
+    #     H = np.reshape(H, (-1, 163842))
+    #     H = H[:, :self.level].astype(np.float32)
+    #     H = np.expand_dims(H, 1)    # should be expanded if multi-channel
+    #     return H
+    #
+    #
+    # def process(self, i, name, data_dir, channel_name, H):
+    #     if i % 5 == 0:
+    #         print(i)
+    #     f = join(data_dir, name) + channel_name
+    #     H[i] = np.loadtxt(fname=f)
+    #     return H
+
+    def k_folds(self, CV, TD, ASD, size_v=1, size_t=1):
+        k = 10
+        S_ASD = int(len(ASD) / k)
+        S_TD = int(len(TD) / k)
+        P_ASD = []
+        P_TD = []
+        for i in range(k + 1):
+            P_ASD.append(i * S_ASD)
+            P_TD.append(i * S_TD)
+        P_ASD[-1] = len(ASD)
+        P_TD[-1] = len(TD)
+        frames = []
+        frames2 = []
+        frames3 = []
+        for i in range(k - size_v - size_t):
+            idx1 = (CV + i) % k
+            idx2 = idx1 + 1
+            a = ASD.loc[range(P_ASD[idx1], P_ASD[idx2])]
+            b = TD.loc[range(P_TD[idx1], P_TD[idx2])]
+            frames.append(a)
+            frames.append(b)
+        train = pd.concat(frames, ignore_index=False)
+        for i in range(size_t):
+            idx3 = (idx2 + i) % k
+            idx4 = idx3 + 1
+            c = ASD.loc[range(P_ASD[idx3], P_ASD[idx4])]
+            d = TD.loc[range(P_TD[idx3], P_TD[idx4])]
+            frames2.append(c)
+            frames2.append(d)
+        test = pd.concat(frames2, ignore_index=False)
+        for i in range(size_v):
+            idx5 = (idx4 + i) % k
+            idx6 = idx5 + 1
+            e = ASD.loc[range(P_ASD[idx5], P_ASD[idx6])]
+            f = TD.loc[range(P_TD[idx5], P_TD[idx6])]
+            frames3.append(e)
+            frames3.append(f)
+        valid = pd.concat(frames3, ignore_index=False)
+        return train, test, valid
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
